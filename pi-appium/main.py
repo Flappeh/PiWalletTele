d56de791987b6a5974f6b85ab24813aefdbb9ae9
flappeh <bonar_david@rocketmail.com>
2024-08-31 14:58:26 +0700

    update to latest

--- pi-appium/main.py
+++ pi-appium/main.py
@@ -11,6 +11,8 @@ CURRENT_PAGE_SOURCE=""
 capabilities: Dict[str, Any] = {
     "platformName": "Android",
     "automationName": "uiautomator2",
+    # "appPackage":"pi.browser", 
+    # "appActivity": "com.pinetwork.MainActivity",
     "deviceName": "Android",
     "language": "en"
 }
@@ -24,12 +26,20 @@ driver = webdriver.Remote(appium_server_url, options=AppiumOptions().load_capabi
 # parent: WebElement = el.parent
 # el.click()
 
+def click_update():
+    try:
+        update = driver.find_element(by=AppiumBy.XPATH, value='//*[@resource-id="mui-3"]')
+        update.click()
+    except:
+        print("error")
+        pass
+
 def click_notif():
-        try:
-            notif = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Not Now"]')
-            notif.click()
-        except:
-            pass
+    try:
+        notif = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Not Now"]')
+        notif.click()
+    except:
+        pass
     
 
 def check_is_loading():
@@ -47,6 +57,10 @@ def check_current_page():
         return "loading"
     if "Unlock Pi Wallet" in CURRENT_PAGE_SOURCE:
         return "wallet_home"
+    if "Turn on notifications!" in CURRENT_PAGE_SOURCE:
+        return "notification"
+    if "Update your app" in CURRENT_PAGE_SOURCE:
+        return "update"
     if "History" in CURRENT_PAGE_SOURCE and "Migrations" in CURRENT_PAGE_SOURCE and "Wallet" in CURRENT_PAGE_SOURCE:
         return "wallet_page"
 
@@ -67,21 +81,32 @@ def reload_wallet_page():
         pass
     
 
-def login_with_passphrase(pwd: str):
+def navigate_to_wallet_home():
+    while check_current_page() != "wallet_home":    
+        match check_current_page():
+            case "wallet_home":
+                break
+            case "wallet_page":
+                reload_wallet_page()
+            case "loading":
+                print("Loading")
+            case "notification":
+                print("Notification Found")
+                click_notif()
+            case "update":
+                print("Update notification found")
+                click_update()
+            case _:
+                print("Undefined")
+                return
+            
+    
+
+def open_wallet_from_passphrase(pwd: str):
     current_page = check_current_page()
-    match current_page:
-        case "wallet_home":
-            pass
-        case "wallet_page":
-            reload_wallet_page()
-        case "loading":
-            print("Loading")
-        case _:
-            print("Undefined")
-            return
     if check_current_page() != "wallet_home":
         print("Not in wallet page")
-        return
+        navigate_to_wallet_home()
     try:
         login_box = driver.find_element(by=AppiumBy.XPATH, value="//android.widget.EditText")
         button_login = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Unlock With Passphrase"]')
@@ -97,6 +122,6 @@ def login_with_passphrase(pwd: str):
     
     
     
-login_with_passphrase("coyote arctic million tattoo tenant onion retreat upon cat avoid correct suspect trick century hard mango song mimic space kingdom volume wealth scan attack")
+open_wallet_from_passphrase("coyote arctic million tattoo tenant onion retreat upon cat avoid correct suspect trick century hard mango song mimic space kingdom volume wealth scan attack")
 
 driver.quit()
